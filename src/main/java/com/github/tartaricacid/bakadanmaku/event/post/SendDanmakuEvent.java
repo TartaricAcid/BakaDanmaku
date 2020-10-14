a50809f06b30b482fca6c6270af2a263f983efc2
TartaricAcid <baka943@qq.com>
2020-10-14 12:42:49 +0800

    1.10.2 第一版

---
+++ src/main/java/com/github/tartaricacid/bakadanmaku/event/post/SendDanmakuEvent.java
@@ -0,0 +1,30 @@
+package com.github.tartaricacid.bakadanmaku.event.post;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber
+public class SendDanmakuEvent extends Event {
+    private final String message;
+
+    public SendDanmakuEvent(String message) {
+        this.message = message;
+    }
+
+    @SubscribeEvent
+    public static void onSendDanmaku(SendDanmakuEvent event) {
+        Minecraft minecraft = Minecraft.getMinecraft();
+        EntityPlayerSP player = minecraft.thePlayer;
+        if (player != null) {
+            player.addChatMessage(new TextComponentString(event.getMessage()));
+        }
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/com/github/tartaricacid/bakadanmaku/websocket/WebSocketClient.java
@@ -0,0 +1,77 @@
+package com.github.tartaricacid.bakadanmaku.websocket;
+
+import com.github.tartaricacid.bakadanmaku.BakaDanmaku;
+import com.github.tartaricacid.bakadanmaku.site.ISite;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+
+import java.net.URI;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class WebSocketClient {
+    public static final ScheduledExecutorService SERVICE = Executors.newSingleThreadScheduledExecutor();
+    private final ISite site;
+    private final URI uri;
+    private Channel channel;
+
+    public WebSocketClient(ISite site) {
+        this.site = site;
+        this.uri = URI.create(site.getUri());
+    }
+
+    public void open() throws Exception {
+        Bootstrap bootstrap = new Bootstrap();
+        SslContext sslCtx = SslContext.newClientContext(InsecureTrustManagerFactory.INSTANCE);
+        EventLoopGroup group = new NioEventLoopGroup();
+        WebSocketClientHandshaker handshaker = WebSocketClientHandshakerFactory.newHandshaker(uri, WebSocketVersion.V13,
+                null, false, HttpHeaders.EMPTY_HEADERS);
+        WebSocketClientHandler handler = new WebSocketClientHandler(handshaker, site);
+
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) {
+                        ChannelPipeline pipe = ch.pipeline();
+                        pipe.addLast(
+                                sslCtx.newHandler(ch.alloc(), uri.getHost(), uri.getPort()),
+                                new HttpClientCodec(),
+                                new HttpObjectAggregator(8192),
+                                handler
+                        );
+                    }
+                });
+
+        channel = bootstrap.connect(uri.getHost(), uri.getPort()).sync().channel();
+        handler.handshakeFuture().sync();
+
+        site.initMessage(this);
+        BakaDanmaku.HEART_BEAT_TASK = SERVICE.scheduleAtFixedRate(() -> sendMessage(site.getHeartBeat()),
+                site.getHeartBeatInterval(), site.getHeartBeatInterval(), TimeUnit.MILLISECONDS);
+    }
+
+    public void close() throws InterruptedException {
+        channel.writeAndFlush(new CloseWebSocketFrame());
+        channel.closeFuture().sync();
+        BakaDanmaku.HEART_BEAT_TASK.cancel(true);
+    }
+
+    public void sendMessage(final ByteBuf binaryData) {
+        channel.writeAndFlush(new BinaryWebSocketFrame(binaryData));
+    }
+}
