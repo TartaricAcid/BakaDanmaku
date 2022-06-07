802ecdb87530fb446d5530ba0d90f7f72b3d7f88
TartaricAcid <baka943@qq.com>
2022-06-07 16:02:28 +0800

    更新到 1.18.2

---
+++ src/main/java/com/github/tartaricacid/bakadanmaku/event/post/SendDanmakuEvent.java
@@ -0,0 +1,32 @@
+package com.github.tartaricacid.bakadanmaku.event.post;
+
+
+import net.minecraft.Util;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class SendDanmakuEvent extends Event {
+    private final String message;
+
+    public SendDanmakuEvent(String message) {
+        this.message = message;
+    }
+
+    @SubscribeEvent
+    public static void onSendDanmaku(SendDanmakuEvent event) {
+        Minecraft minecraft = Minecraft.getInstance();
+        LocalPlayer player = minecraft.player;
+        if (player != null) {
+            player.sendMessage(new TextComponent(event.getMessage()), Util.NIL_UUID);
+        }
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
