33504f91756bafb034e4b857c5e99ebd8d3249ba
Aya-Net <94779206+Aya-Net@users.noreply.github.com>
2022-09-29 23:13:54 +0800

    Update to support 1.19 and 1.19.x
    Use Java Reflect to invoke API in 1.19.x

--- src/main/java/com/github/tartaricacid/bakadanmaku/event/post/SendDanmakuEvent.java
+++ src/main/java/com/github/tartaricacid/bakadanmaku/event/post/SendDanmakuEvent.java
@@ -4,7 +4,6 @@ package com.github.tartaricacid.bakadanmaku.event.post;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
-import net.minecraft.network.chat.ChatSender;
 import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.eventbus.api.Event;
@@ -22,7 +21,12 @@ public class SendDanmakuEvent extends Event {
     @SubscribeEvent
     public static void onSendDanmaku(SendDanmakuEvent event) {
         Minecraft minecraft = Minecraft.getInstance();
-        if (minecraft.gui != null) {
+        try {
+            Object chatListener = minecraft.getClass().getMethod("m_240442_").invoke(minecraft);
+            // ChatListener chatListener = minecraft.getChatListener();
+            chatListener.getClass().getMethod("m_240494_", Component.class, boolean.class).invoke(chatListener, Component.literal(event.getMessage()), false);
+            // chatListener.handleSystemChat(Component.literal(event.getMessage()), false);
+        } catch (Exception e) {
             Registry<ChatType> chatTypes = RegistryAccess.BUILTIN.get().registryOrThrow(Registry.CHAT_TYPE_REGISTRY);
             ChatType chatType = chatTypes.get(ChatType.CHAT);
             if (chatType != null) {
